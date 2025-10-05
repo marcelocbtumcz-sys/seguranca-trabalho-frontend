f6146e3f5951b5a954e7d65f9e2db6b0fbe0e816
marcelocbtumcz-sys <marcelo.cbtu.mcz@gmail.com>
2025-10-04 21:27:16 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
-// config.js
+esse aqui tem que ajustar?  // config.js
 // 🔹 Endereço base da sua API
-// Quando estiver no Render, use o domínio gerado automaticamente.
-// Exemplo: "https://seguranca-backend.onrender.com"
-const API_BASE = window.location.hostname.includes("localhost")
-  ? "http://localhost:3000" // ✅ Usa local quando estiver rodando na sua máquina
-  : "https://seguranca-trabalho-backend.onrender.com"; // ✅ Link real do Render
+// Troque o IP se rodar em outro servidor
+// Se quiser usar localmente, pode colocar "http://localhost:3000"
+const API_BASE = "https://seguranca-trabalho-backend.onrender.com";
+
+
+
 
 
 
